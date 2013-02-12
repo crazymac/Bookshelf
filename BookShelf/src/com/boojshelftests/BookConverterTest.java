97bb167fd60270904b67c64d3de354173f13a998
crazy_mac <lildee1991@gmail.com>
2013-02-12 10:10:52 +0300

    Book -> List<Column> test

--- BookShelf/src/com/boojshelftests/BookConverterTest.java
+++ BookShelf/src/com/boojshelftests/BookConverterTest.java
@@ -1,5 +1,18 @@
 package com.boojshelftests;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
+import org.junit.Test;
+
+import com.bookshelf.Book;
+
 public class BookConverterTest {
 
+	@Test
+	public void converterTest() throws FileNotFoundException{
+		
+		Book book = Book.getInstance();
+		book.newBook(1, "CassandraTest", "Test", "Tester", new FileInputStream("resources/testbook"));
+	}
 }
